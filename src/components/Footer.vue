6bf46a483172e30d4fc8c3cc3612b35e7ff8cb20
Maria-GF <m.g.f@live.com>
2024-01-14 21:48:13 +0100

    css finished

---
+++ src/components/Footer.vue
@@ -0,0 +1,11 @@
+<script setup>
+
+</script>
+<template>
+    <div>
+        <h1 class="&COPY COPYRIGHT MGF">COPYRIGHT MGF</h1>
+    </div>
+</template>
+
+
+
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,5 +2,27 @@
 
 </script>
 <template>
-
+     <header class="header">
+    <div class="header__container">
+        <div class="header__bar">
+            <a href="/">
+                <img src="/img/logo.png" class="header__logo" alt="logo">
+            </a>
+            <nav class="navigation">
+                <a class="navigation__link" href="/">Inicio</a>
+                <a class="navigation__link" ref="/">Nosotros</a>
+                <a class="navigation__link" ref="/">Blog</a>
+                <a class="navigation__link" ref="/">Tienda</a>
+            </nav>
+            <img src="/img/carrito.png" alt="" class="cart">
+        </div><!-- header navigation -->
+        <div class="model">
+            <h1 class="model__name">Barca</h1>
+            <p class="model__description">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+            <p class="model__price">29€</p>
+            <a href="/" class="model__link">Añadir producto</a>
+        </div>
+</div>
+<img src="/img/fbarca.png" alt="barca" class="header__barca">
+</header>
 </template>
